683c19bcdf628f7114f11d4a14a4bf559177525a
dennis <mrvon@qq.com>
2025-12-05 14:46:47 +0800

    update

---
+++ test/main.lua
@@ -0,0 +1,25 @@
+local start = require "test.start"
+start {
+    core = {
+        debuglog = "=", -- stdout
+        worker = 3, -- avoid stuck when running ci on GHA macOS
+    },
+    service_path = "service/?.lua;test/?.lua",
+    bootstrap = {
+        -- {
+        --     name = "timer",
+        --     unique = true,
+        -- },
+        -- {
+        --     name = "logger",
+        --     unique = true,
+        -- },
+        -- {
+        --     name = "sockevent",
+        --     unique = true,
+        -- },
+        -- {
+        --     name = "bootstrap",
+        -- },
+    },
+}
